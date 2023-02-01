84e04282a6d0a05d5269099bf09df4470652f563
Harigovind C B <am.en.u4aie22119@am.students.amrita.edu>
2023-02-01 12:29:20 +0530

    Update 2_Recognise_Symbols.py

--- 2_Recognise_Symbols.py
+++ 2_Recognise_Symbols.py
@@ -1,9 +1,12 @@
 # This is the code for making a file which holds all the symbols and variables in the code.
 # This is part of the white.py code as an asm file with no white spaces is required for this code.
 def check(z):
+    # Checking whether it is a variable
     if z[0] == '@':
+        # Checking whether it is a variable or number  
         if z[1].isalpha():
             return True
+    # Checking whether it is a label
     elif z[0] == '(':
         return True
         
@@ -12,9 +15,6 @@ def check(z):
 with open("white.asm",'r') as a:
     with open('symbolsandvariables.asm', 'w+') as b:
         file = a.readlines()
-        m=len(file)
-        n=1
         for i in file:
-            n+=1
             if check(i) :
             	b.write(i)
