5402a5c1cc1bee4708d34e3e14224b4c989e49bb
Viserion-7 <harigovindbiju@gmail.com>
2023-02-16 20:50:33 +0530

    whitespace

--- 1_White.py
+++ 1_White.py
@@ -1,19 +1,19 @@
 # This is the standalone code for removing white spaces from an asm file.
 # change file name here for different asm files
 # current file used is from project 6/rect/rect.asm
-f=input("Enter filename with extension : ")
-with open(f, 'r') as a:
-    with open('white.asm', 'w+') as b:
-        file=a.readlines()
-        for l in file:
-            l=l.replace(' ','')	
+filename=input("Enter filename with extension : ")
+with open(filename, 'r') as raw:
+    with open('white.asm', 'w+') as white:
+        file=raw.readlines()
+        for line in file:
+            line=line.replace(' ','')	
             # removes all spaces
-            if  l[0]=='\n' or l[0]=='/':
+            if  line[0]=='\n' or line[0]=='/':
             # removes new line and comments
                 pass
             else:
-                if '/' in l:
+                if '/' in line:
                 # removes inline comments
-                    x=l.index('/')
-                    l=l[:x]+'\n'
-                b.write(l)
+                    comment_index = line.index('/')
+                    line = line[:comment_index]+'\n'
+                white.write(line)
