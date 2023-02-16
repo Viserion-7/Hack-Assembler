7ee80151a7637df14bcd82e5ebf8a5cf7a876c62
Viserion-7 <harigovindbiju@gmail.com>
2023-02-16 21:02:56 +0530

    Symbols

--- 2_Recognise_Symbols.py
+++ 2_Recognise_Symbols.py
@@ -1,17 +1,18 @@
-# This is the code for making a file which holds all the symbols and variables in the code.
+# This is the code for making a file which holds all the labels and variables in the code.
 # This is part of the white.py code as an asm file with no white spaces is required for this code.
 
 # I have named the file white.asm in the previous code so continuing with the same name here :)
 
-with open("white.asm",'r') as a:
-    with open('symbolsandvariables.asm', 'w+') as b:
-        file = a.readlines()
-        for i in file:
+with open("white.asm",'r') as white:
+    with open('symbolsandvariables.asm', 'w+') as out:
+        # For adding all the new labels and variables found in the asm file
+        sym=[]
+        file = white.readlines()
+        for line in file:
             # Checking whether it is an A instruction
-            if i[0] == '@':
-                # Checking whether it is a variable or number  
-                if i[1].isalpha():
-                    b.write(i)
-            # Checking whether it is a label
-            elif i[0] == '(':
-                b.write(i)
+            if line[0] == '@':
+                # Checking whether it is a symbol or number  
+                if line[1].isalpha():
+                    if line[1:-1] not in sym:
+                        sym.append(line[1:-1])
+                        out.write(line[1:])
